e1c649214e35e0dffb5aec0e73291d54728c227f
harutowhite995-svg <harutowhite995@gmail.com>
2026-07-30 14:54:01 +0500

    Fix stylesheet loading

--- src/main.js
+++ src/main.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { ArrowRight, BadgeCheck, Brush, Clapperboard, Gamepad2, Layers3, Sparkles, Star, MessageCircle, Wand2, Zap } from 'lucide-react';
-import './styles.css';
 
 const h = React.createElement;
 
